1ff20f6a2262d2d64a4d608e2b8b6483ab365927
sanjuesc <58093412+sanjuesc@users.noreply.github.com>
2022-05-18 09:32:08 +0200

    Se recuperan los detalles de los paciente (falta la screen)

--- lib/src/screens/mis_detalles.dart
+++ lib/src/screens/mis_detalles.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
-
+import '../globals.dart' as globals;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
 
 class MisDetalles extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: MiDrawer(),
-      appBar: AppBar(title: Text("eTorax 2.0"),),
-      body: Text('Mis datos'),
+    return FutureBuilder(
+      builder: (context, AsyncSnapshot<Map<String, dynamic>> datosSnap){
+        if (!datosSnap.hasData){
+          return Scaffold(
+            drawer: MiDrawer(),
+            appBar: AppBar(title: Text("eTorax 2.0"),),
+            body: Text('No hay info'),
+          );
+        }else{
+          return Scaffold(
+            drawer: MiDrawer(),
+            appBar: AppBar(title: Text("eTorax 2.0"),),
+            body: Text(datosSnap.data!['mensaje'].toString()),
+          );
+        }
+      },
+      future: completar(context),
     );
+  }
+
+
+
+  Future<Map<String, dynamic>> completar(BuildContext context) async {
+    Client client = Client();
+    final respuesta = await client.post(
+      Uri.parse('http://'+dotenv.env['server']!+'/paciente/detalles'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'access-token': globals.token,
+      },
+      body: jsonEncode(<String, String>{
+        'pacId': globals.usuario,
+      }),
+    );
+    Map<String, dynamic> data = jsonDecode(respuesta.body);
+    if(data['token']!=null) {
+      globals.token = data['token'];
+    }
+    return data;
   }
 
 
